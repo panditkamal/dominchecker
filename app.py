002b687d2c814c407343e2e3bb219b8ef742aa47
panditkamal <kamalsharma@gmail.com>
2025-07-06 11:58:01 +0530

    Set up for Railway deployment with Gunicorn

--- app.py
+++ app.py
@@ -86,8 +86,7 @@ def index():
         result = analyze_domain(domain)
     return render_template("index.html", result=result)
 
+
 # if __name__ == "__main__":
-#     app.run(debug=True)
-if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=5000)
+#     app.run(debug=False, host='0.0.0.0', port=5000)
 
